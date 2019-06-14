a3dbe9516842378891d2ada2425253787ca951f3
Brandon Istenes <bistenes@gmail.com>
2019-06-14 14:28:55 -0500

    Get export_database working, with the right text

--- src/export_database.py
+++ src/export_database.py
@@ -12,15 +12,23 @@ DEBUG = False
 DB_HOST = "localhost"
 DB_USER = "root"
 
+MSG_APP_NAME = "Exportar Baso de Datos"
+MSG_NEXT = "Siguiente"
+MSG_DONE = "Ya!"
+MSG_PASSWORD = "Contraseña"
+
+MSG_EXPORT_PASSWORD = "Por favor, ingresa la contraseña de usuario “root” de MySQL. También necesitamos la contraseña para cifrar el archivo."
+MSG_EXPORT_DATABASE_SELECT = "Selecciona la base de datos para exportar."
+MSG_EXPORT_SAVE_FILE = "Elige un lugar para guardar el archivo."
+
 
 def debug(*args, **kwargs):
     if DEBUG:
         print(*args, **kwargs)
 
 
-class MainApplication(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
+class MainApplication(object):
+    def __init__(self, parent):
 
         self.parent = parent
         self.step = 0
@@ -28,9 +36,10 @@ class MainApplication(tk.Frame):
         self.database = None
         self.filename = None
 
-        self.do_step()
+        self.parent.withdraw()
+        self._do_step()
 
-    def do_step(self):
+    def _do_step(self):
         debug("do_step on step {}".format(self.step))
         if self.step == 0:
             self.get_password()
@@ -47,34 +56,43 @@ class MainApplication(tk.Frame):
                 "Somehow wound up on non-existant step {}".format(self.step)
             )
 
-    def go_to_next_step(self):
+    def _go_to_next_step(self):
         debug("go_to_next_step")
         self.step += 1
-        self.do_step()
+        self._do_step()
+
+    def _get_new_frame(self, title=MSG_APP_NAME):
+        frame = tk.Toplevel(self.parent)
+        frame.geometry("600x400")
+        return frame
 
     def get_password(self):
         # Get password
-        self.parent.withdraw()
         self.password = tkinter.simpledialog.askstring(
-            "Password", "Enter password:", show="*"
+            MSG_PASSWORD, MSG_EXPORT_PASSWORD, show="*"
         )
-        self.go_to_next_step()
+        self._go_to_next_step()
 
     def select_database(self):
         debug("select_database")
-        self.parent.deiconify()
         databases = get_database_list(self.password)
-        database_choice = tk.StringVar(self.parent)
+
+        frame = self._get_new_frame()
+
+        w = tk.Label(frame, text=MSG_EXPORT_DATABASE_SELECT)
+        w.pack()
+
+        database_choice = tk.StringVar(frame)
         database_choice.set(databases[0])  # default value
-        w = tk.OptionMenu(self.parent, database_choice, *databases)
+        w = tk.OptionMenu(frame, database_choice, *databases)
         w.pack()
 
         def set_database():
             self.database = database_choice.get()
-            self.parent.withdraw()
-            self.go_to_next_step()
+            frame.destroy()
+            self._go_to_next_step()
 
-        button = tk.Button(self.parent, text="OK", command=set_database)
+        button = tk.Button(frame, text="OK", command=set_database)
         button.pack()
 
     def select_file(self):
@@ -82,18 +100,24 @@ class MainApplication(tk.Frame):
         self.filename = tkinter.filedialog.asksaveasfilename(
             initialdir=home,
             initialfile="openmrs-dump",
-            title="Select save file",
+            title=MSG_EXPORT_SAVE_FILE,
             defaultextension=".zip",
             filetypes=(("zip file", "*.zip"),),
         )
-        self.go_to_next_step()
+        self._go_to_next_step()
 
     def execute(self):
         debug("execute")
-        print(self.password)
-        print(self.database)
-        print(self.filename)
-        print("Dumping...")
+        debug(self.password)
+        debug(self.database)
+        debug(self.filename)
+
+        frame = self._get_new_frame()
+
+        w = tk.Label(frame, text="Dumping...")
+        w.pack()
+        frame.update()
+
         temp_dir = tempfile.TemporaryDirectory()
         sql_path = str(Path(temp_dir.name) / (self.database + ".sql"))
         dumpcmd = (
@@ -109,11 +133,19 @@ class MainApplication(tk.Frame):
             + sql_path
         )
         os.system(dumpcmd)
-        print("Zipping...")
+
+        w = tk.Label(frame, text="Zipping...")
+        w.pack()
+        frame.update()
+
         gzipcmd = "gzip -c " + sql_path + "> " + self.filename
         os.system(gzipcmd)
-        print("Done!")
-        self.go_to_next_step()
+
+        w = tk.Label(frame, text=MSG_DONE)
+        w.pack()
+
+        button = tk.Button(frame, text="OK", command=self._go_to_next_step)
+        button.pack()
 
 
 def get_database_list(mysql_parent_password):
@@ -128,5 +160,5 @@ def get_database_list(mysql_parent_password):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    MainApplication(root).pack(side="top", fill="both", expand=True)
+    MainApplication(root)
     root.mainloop()
